25e50e541e5dbde9a8771989b27d9f4c01427166
Salvador P <salvadorperezd@gmail.com>
2023-12-10 23:10:39 +0100

    Codigo inicial

---
+++ Ejercicio 7 Tema 4/Ejercicio 7 Tema 4/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Ejercicio_7_Tema_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mcdBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(328, 86);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(142, 26);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(328, 163);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(142, 26);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Número 2:";
+            // 
+            // mcdBtn
+            // 
+            this.mcdBtn.BackColor = System.Drawing.Color.DeepPink;
+            this.mcdBtn.Location = new System.Drawing.Point(310, 247);
+            this.mcdBtn.Name = "mcdBtn";
+            this.mcdBtn.Size = new System.Drawing.Size(126, 53);
+            this.mcdBtn.TabIndex = 4;
+            this.mcdBtn.Text = "Calcular MCD";
+            this.mcdBtn.UseVisualStyleBackColor = false;
+            this.mcdBtn.Click += new System.EventHandler(this.mcdBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Plum;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mcdBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 7 Tema 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button mcdBtn;
+    }
+}
+
---
+++ Ejercicio 7 Tema 4/Ejercicio 7 Tema 4/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_7_Tema_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Una función para saber el número menor.
+        int calculaMenor(int num1, int num2)
+        {
+            int res = num2;
+            if (num1 <= num2)
+            {
+                res = num1;
+            }
+            return res;
+        }
+        //Una función que calcule el MCD.
+        int calculaMCD(int num1, int num2)
+        {
+            int mcd = 1;
+
+            int menor = calculaMenor(num1, num2);
+
+            int mayor = num1;
+            if (menor == num1)
+            {
+                mayor = num2;
+            }
+            for (int i = 1; i <= menor; i++)
+            {
+                if (menor % i == 0 && mayor % i == 0)
+                {
+                    mcd = i;
+                }
+            }
+            return mcd;
+        }
+        private void mcdBtn_Click(object sender, EventArgs e)
+        {
+            int num1;
+            int num2;
+            int resultado;
+
+            num1 = int.Parse(txtNum1.Text);
+            num2 = int.Parse(txtNum2.Text);
+
+            resultado = calculaMCD(num1, num2);
+
+            MessageBox.Show("El MCD de " + num1 + " y " + num2 + " es " + resultado);
+        }
+    }
+}
